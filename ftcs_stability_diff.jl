0789d994d5eee2a306e520bfbbd3b18fe4d5b3e0
Vini-BM <vinicius.bayne@ufrgs.br>
2024-03-27 19:41:54 -0300

    FTCS stability

--- ftcs_stability_diff.jl
+++ ftcs_stability_diff.jl
@@ -27,29 +27,26 @@ L = 100 # xmax
 x = [i for i in 0:L] # space interval
 println(x)
 tf = 20 # time interval
-k_list = [.4, .5, .6, .7] # parameter to be tested
-plot_list = Vector{Any}(undef,length(k_list))
+k_list = [.3, .5, .7, .9] # parameter to be tested
+plot_list = []
 ρ0 = 1 # initial condition
 
 # Initialization
 ρ0 = zeros(L+1) # initializes \rho as zeros
 ρ0[1] = 1 # sets initial condition [1, 0, ... , 0]
 println(ρ0)
-#plot(x,ρ0,label="t=0",ls=:dash,dpi=1000)
 
 # Integration
-for i in 1:length(k_list)
+for i=1:length(k_list)
     k = k_list[i]
     ρ = ftcs(ρ0,tf,k)
     println(ρ)
-    plot_list[i] = plot(x,ρ,title="k=$k",dpi=1000)
+    push!(plot_list,plot(x,ρ,title="k=$k",legend=false,color=i,dpi=1000))
 end
 
 # Plot
-plot!(plot_list,legend=:bottomright,plot_title="FTCS stability with diffusion equation | t=$tf")
-#plot!(legend=:bottomright)
-#title!("FTCS stability with diffusion equation | t=$tf")
+plot!(plot_list...,plot_title="FTCS stability | t=$tf")
 xlabel!("x")
 ylabel!("ρ")
 readline() # keep plot open
-#savefig("Plots/ftcs_stability.png")
+savefig("Plots/ftcs_stability.png")
