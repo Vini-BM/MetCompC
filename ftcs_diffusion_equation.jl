dcb131508e01365a45124d112769cc7326df272f
Vini-BM <vinicius.bayne@ufrgs.br>
2024-03-28 16:40:48 -0300

    Corrections and time

--- ftcs_diffusion_equation.jl
+++ ftcs_diffusion_equation.jl
@@ -15,16 +15,15 @@ function ftcs(f,ti,tf,dt=0.1)
     t = ti # initializes time
     while t < tf # loop
         t+=dt # increase time
-        f[2:L-1] += k*(f[3:L] + f[1:L-2] - 2f[2:L-1]) # ftcs discretization
+        f[2:end-1] += k*(f[3:end] + f[1:end-2] - 2f[2:end-1]) # ftcs discretization
         # Extreme values are treated as boundary conditions
     end
-    return f
+    #return f -> not necessary because f is global
 end
 
 # Parameters
 L = 100 # xmax
 x = [i for i in 0:L] # space interval
-println(x)
 tf_list = [0, 25, 50, 75, 100] # time interval
 k = 0.5 # k = D dt/dx^2 (k<=0.5 for FTCS), D: diffusion coefficient
 ρ0 = 1 # initial condition
@@ -32,16 +31,14 @@ k = 0.5 # k = D dt/dx^2 (k<=0.5 for FTCS), D: diffusion coefficient
 # Initialization
 ρ = zeros(L+1) # initializes \rho as zeros
 ρ[1] = ρ0 # sets initial condition [1, 0, ... , 0]
-println(ρ)
 plot!(x,ρ,label="t=0",dpi=1000)
 
 # Integration
 for i in 2:lastindex(tf_list)
     tf = tf_list[i]
     ti = tf_list[i-1]
-    global ρ = ftcs(ρ,ti,tf)
-    println(ρ)
-    plot!(x,ρ,label="t=$tf",dpi=1000)
+    @time ftcs(ρ,ti,tf)
+    plot!(ρ,label="t=$tf",dpi=1000)
 end
 
 # Plot
