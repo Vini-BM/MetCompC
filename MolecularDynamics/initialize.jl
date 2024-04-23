af97b7528b35fe03516b546b37e3baeeaed36473
Vini-BM <vinicius.bayne@ufrgs.br>
2024-04-23 17:11:00 -0300

    Begin study on molecular dynamics

---
+++ MolecularDynamics/initialize.jl
@@ -0,0 +1,18 @@
+using Distributions
+using Plots
+
+function inicia(N,temp)
+    X = 10
+    Y = 10
+    N = Int(N)
+    dt = 0.1
+    x = X*rand(N)
+    y = Y*rand(N)
+    d = Normal(0,sqrt(temp)) # mean=0, var=temp -> d is a distribution 'object', not a random variable
+    vx = rand(d,N) # generates a N-vector with random values distributed according to d
+    vy = rand(d,N)
+    Km = sum(vx.^2 + vy.^2)/(2*N) # mean kinetic energy -> using Kb = m = 1, Km = temp
+    println("Mean Kinetic Energy: $Km")
+    return x, y, vx, vy, temp, dt
+end
+
