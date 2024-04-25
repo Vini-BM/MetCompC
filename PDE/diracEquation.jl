915b5840c8d231ab0b4eec064677acd1746d8818
Vini-BM <vinicius.bayne@ufrgs.br>
2024-04-25 17:08:07 -0300

    tests

--- PDE/diracEquation.jl
+++ PDE/diracEquation.jl
@@ -8,29 +8,36 @@
 using Plots
 Plots.default(show=true)
 
-function func(f, g, tf, dt, dx)
+function func(f, g, v, vsc, tf, dt, dx)
     t = 0
-    ymin = -.5
+    ymin = 0
     ymax = .5
+    pf = abs2.(f)
+    pfint = sum(pf)*dx
+    pg = abs2.(g)
+    plot([x,x,x],[pf,pg,real(vsc)],ylim=(ymin,ymax),title="t=$t | $pfint")
     while t<tf
         oldf = deepcopy(f)
         oldg = deepcopy(g)
-        f[2:end-1] = .5*(oldf[3:end]+oldf[1:end-2]) - dt/(2*dx)*(oldg[3:end]-oldg[1:end-2]) - im*dt*oldf[2:end-1]
-        g[2:end-1] = .5*(oldg[3:end]+oldg[1:end-2]) - dt/(2*dx)*(oldf[3:end]-oldf[1:end-2]) + im*dt*oldg[2:end-1]
+        f[2:end-1] = .5*(oldf[3:end]+oldf[1:end-2]) - dt/(2*dx)*(oldg[3:end]-oldg[1:end-2]) + im*(v[2:end-1]-(1 .+vsc[2:end-1]))*dt.*oldf[2:end-1]
+        g[2:end-1] = .5*(oldg[3:end]+oldg[1:end-2]) - dt/(2*dx)*(oldf[3:end]-oldf[1:end-2]) + im*(v[2:end-1]+(1 .+vsc[2:end-1]))*dt.*oldg[2:end-1]
         #f[1] = 0
         #f[end] = 0
         #g[1] = 0
         #g[end] = 0
         t += dt
-        plot([x,x],[real(f),imag(f)],ylim=(ymin,ymax),title="$dx")
+        pf = abs2.(f)
+        pg = abs2.(g)
+        pfint = sum(pf)*dx
+        plot([x,x,x],[pf,pg,real(vsc)],ylim=(ymin,ymax),title="t=$t | $pfint")
         #plot([x,x],[abs2.(f),abs.(g)],ylim=(0,ymax))
     end
     return f, g
 end
 
 # Parameters
-xmax = 100
-dx = .5
+xmax = 200
+dx = 1
 x = collect(1:dx:xmax)
 L = length(x)
 dt = .1
@@ -38,30 +45,36 @@ dt = .1
 t = 0
 
 # Gaussian source
-sigma = 3 # std
+sigma = 6 # std
 center = L/2 # center of gaussian
 f = zeros(L) # initialize array
 @. f = exp(-(x-center)^2 / 2sigma^2) # broadcasting
 f = complex(f)
-println(f)
+#println(f)
 # g
 g = zeros(L)
 g = complex(g)
-
+# v
+vsc = zeros(L)
+vsc[110] =  50
+vsc[90] = 50
+vsc = complex(vsc)
+v = zeros(L)
+v = complex(v)
 # Plot
 #plot(x,real(f))
 #plot!(x,g)
 
 
-tf = 15
-f, g = func(f,g,tf,dt,dx)
+tf = 30
+f, g = func(f,g,v,vsc,tf,dt,dx)
 #for dx in dxlist
 #f0 = copy(f)
 #g0 = copy(g)
 #ff, gf = func(f0,g0,tf,dt,dx)
 #end
 #plot!(x,real(f))
-#readline()
+readline()
 
 
 
