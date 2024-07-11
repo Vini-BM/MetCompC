322463d06f291c858ae034540fa67e85adb612c6
Vini-BM <vinicius.bayne@ufrgs.br>
2024-07-11 17:06:29 -0300

    testing class script

--- MolecularDynamics/din-mol-boxes.jl
+++ MolecularDynamics/din-mol-boxes.jl
@@ -44,9 +44,9 @@ end
 function inicia()
     sqN=10
     N=sqN^2
-    Temp=0.001
+    T=0.001
     Random.seed!(1234)
-    x,y,vx,vy,X,Y=init_pos_vel(sqN,Temp)
+    x,y,vx,vy,X,Y=init_pos_vel(sqN,T)
     xv = Coord(x,y,vx,vy)
     N=length(xv.x)
     X,Y,box=init_boxes(X,Y,N)
@@ -56,11 +56,11 @@ function inicia()
     return xv,dynpar,box
 end
 
-function init_pos_vel(sqN,Temp)
+function init_pos_vel(sqN,T)
     N = sqN^2
     x = Array{Float64,1}(undef,N)
     y = Array{Float64,1}(undef,N)
-    d=Normal(0,sqrt(Temp))
+    d=Normal(0,sqrt(T))
     ddx = 0.5  #initial distance between particles along X 
     ddy = 0.5  #initial distance between particles along Y
     X = ddx*(sqN+1) #define X size
@@ -81,8 +81,8 @@ function init_pos_vel(sqN,Temp)
     end
     vx=rand(d,N)
     vy=rand(d,N)
-    #vx .= 0.0
-    #vy .= 0.0
+    vx .= 0.0
+    vy .= 0.0
     return x,y,vx,vy,X,Y
 end
 
@@ -104,7 +104,7 @@ function init_boxes(X,Y,N)
     for j=2:Ly-1
         for i=2:Lx-1
             c=ncel[i,j]
-            v1[c]=ncel[i,j+1]   #right neighbor
+            v1[c]=ncel[i,j]+1   #right neighbor
             v2[c]=ncel[i+1,j-1]#left below neighbor
             v3[c]=ncel[i+1,j]  #below neighbor
             v4[c]=ncel[i+1,j+1]#right below neighbor
@@ -116,7 +116,7 @@ function init_boxes(X,Y,N)
     [(c=ncel[i,Lx];v1[c]=nf;v2[c]=ncel[i+1,Lx-1];v3[c]=ncel[i+1,1];v4[c]=nf) for i=1:Ly-1] 
     #neighbors of the last line
     [(c=ncel[Ly,j];v1[c]=ncel[Ly,j+1];v2[c]=nf;v3[c]=nf;v4[c]=nf) for j=1:Ly-1]
-    lista=zeros(nf,ceil(Integer,2*N/nc))
+    lista=zeros(nf,ceil(Integer,4*N/nc))
     num=zeros(nc)
     box = boxes(nc,nf,Lx,Ly,v1,v2,v3,v4,ncel,lista,num)
     return X,Y,box
@@ -153,84 +153,80 @@ function contorno(xv,dynpar)
     xv.vx[zx] .= -xv.vx[zx]
     zy=findall(t->(t<0)||(t>dynpar.Y),xv.y)
     xv.vy[zy] .= -xv.vy[zy]
-    return xv
+return xv
 end
 
-function force_cells_intra(xv,dynpar,f,box)
-    f.fx .= 0.0 ; f.fy = 0.0
+function forces_cells_intra(xv,dynpar,f,box)
+    f.fx .= 0.0 ; f.fy .= 0.0
     #in box forces
     for c=1:box.nc
-        n=num[c]  #particles in box c
-        if nc > 1
+        n=box.num[c]  #particles in box c
+        if box.nc > 1
             fpx=zeros(n,n)
             fpy=zeros(n,n)
             listc=box.lista[c,1:n]
             for k=1:n-1
-                #p=[k+1:n;]
-                for p in k+1:n
-                    nk=listc[k]    #identity of particle k
-                    np=listc[p]    #identity of particle p
-                    xij=xv.x[nk]-xv.x[np]
-                    yij=xv.y[nk]-xv.y[np]
-                    rij=sqrt(xij^2+yij^2)
-                    if rij < dynpar.ra
-                        fmod = -dynpar.k0*(rij-dynpar.r0)
-                        f.fx[k]+=fmod*xij/rij
-                        f.fy[k]+=fmod*yij/rij
-                        f.fx[p]-=fmod*xij/rij
-                        f.fy[p]-=fmod*yij/rij
-                    end
-		        end
-	        end
-        end
-	end
-    return f
-end
-
-function force_cells_inter(xv,dynpar,f,box)
-# Now we calculate the forces among particles of c and the neighboring boxes
-    for c=1:nc-1
-        n=num[c]  #particles in cell c
-        listv=[]
-        if nc > 0
-            listc=box.lista[c,1:n]
-            n1=num[v1[c]]
-            n2=num[v2[c]]
-            n3=num[v3[c]]
-            n4=num[v4[c]]
-            nv=n1+n2+n3+n4
-            if nv > 0
-                if n1>0 push!(listv,box.lista[v1[c],1:n1])
-                end
-                if n2>0 push!(listv,box.lista[v1[c],1:n2])
-                end
-                if n3>0 push!(listv,box.lista[v1[c],1:n3])
-                end
-                if n4>0 push!(listv,box.lista[v1[c],1:n4])	    
-                end   
-            end
+                p=[k+1:n;]
+                nk=listc[k]    #identity of particle k
+                np=listc[p]    #identity of particle p
+                xij=xv.x[nk]-xv.x[np]
+                yij=xv.y[nk]-xv.y[np]
+                rij=sqrt(xij^2+yij^2)
+                if rij < dynpar.ra
+                    fmod = -dynpar.k0*(rij-dynpar.r0)
+                    f.fx[k]+=fmod*xij/rij
+                    f.fy[k]+=fmod*yij/rij
+                    f.fx[p]-=fmod*xij/rij
+                    f.fy[p]-=fmod*yij/rij
+		end
 	    end
-
-        for i in listc
-	        for j in listv
-	        	xij=xv.x[i]-xv.x[j]
-                	yij=xv.y[i]-xv.y[j]
-                	rij=sqrt(xij^2+yij^2)
-                	if rij<dynpar.ra
-                       fmod = -dynpar.k0*(rij-dynpar.r0)
-                       fx = fmod*xij/rij
-                       fy = fmod*yij/rij
-                       f.fx[i] += fx
-                       f.fy[i] += fy
-                       f.fx[j] -= fx #usando a terceira lei de Newton permite 
-                       f.fy[j] -= fy #que iniciemos o laço interno em i+1
-	                end
-            end
-        end
+	end
      end
      return f
 end
+function forces_cells_inter(xv,dynpar,f,box)
+# Now we calculate the forces among particles of c and the neighboring boxes
+     for c=1:box.nc-1
+     	 n=box.num[c]  #particles in cell c
+	 listv=[]
+	 if box.nc > 0
+	    listc=box.lista[c,1:n]
+	    n1=box.num[box.v1[c]]
+	    n2=box.num[box.v2[c]]
+	    n3=box.num[box.v3[c]]
+	    n4=box.num[box.v4[c]]
+	    nv=n1+n2+n3+n4
+	    if nv > 0
+	       if n1>0 push!(listv,box.lista[box.v1[c],1:n1])
+	       end
+	       if n2>0 push!(listv,box.lista[box.v1[c],1:n2])
+	       end
+	       if n3>0 push!(listv,box.lista[box.v1[c],1:n3])
+	       end
+	       if n4>0 push!(listv,box.lista[box.v1[c],1:n4])	   
+	       end    
+	    end
+	end
 
+        for i in listc
+	    for j in listv
+	    	xij=xv.x[i]-xv.x[j]
+            	yij=xv.y[i]-xv.y[j]
+            	rij=sqrt(xij^2+yij^2)
+            	if rij<dynpar.ra
+                   fmod = -dynpar.k0*(rij-dynpar.r0)
+                   fx = fmod*xij/rij
+                   fy = fmod*yij/rij
+                   f.fx[i] += fx
+                   f.fy[i] += fy
+                   f.fx[j] -= fx #usando a terceira lei de Newton permite 
+                   f.fy[j] -= fy #que iniciemos o laço interno em i+1
+	        end
+            end
+	end
+     end
+     return f
+end
 function force(xv,dynpar,f)
     f.fx .= 0.0 ; f.fy .= 0.0
     N = length(f.fx)
@@ -270,7 +266,7 @@ function PotentialEnergy(xv,dynpar)
 end
 
 function Kinetic(xv)
-    K = sum(xv.vx .^2+xv.vy .^2)/2
+        K = sum(xv.vx .^2+xv.vy .^2)/2
     return K
 end
 
@@ -281,33 +277,29 @@ function Total_Energy(xv,dynpar)
     return V,K,TE
 end
 
-function move(xv,dynpar,f,box)
+function move(xv,dynpar,f)
     fxi=f.fx;fyi=f.fy
     dt=dynpar.dtt
     mdt2=dt^2/2; h=dt/2
     xv.x .+= xv.vx*dt + mdt2*fxi 
     xv.y .+= xv.vy*dt + mdt2*fyi 
-    #f=force(xv,dynpar,f)
-    f = forces_cell_intra(xv,dynpar,f,box)
-    f = forces_cell_inter(xv,dynpar,f,box)
+    f=force(xv,dynpar,f)
     xv.vx .+= h*(fxi+f.fx) -0.95*xv.vx*dt
     xv.vy .+= h*(fyi+f.fy) -0.95*xv.vy*dt
     return xv,f
 end
 
 
-function evol(xv,f,dynpar,box)
+function evol(xv,f,dynpar)
 #    f=force(xv,dynpar,f)
-    xv, box = build_lists(xv,box)
-    f=forces_cell_intra(xv,dynpar,f,box)
-    f=forces_cell_inter(xv,dynpar,f,box)
+    f=forces_cells_intra(xv,dynpar,f,box)
+    f=forces_cells_inter(xv,dynpar,f,box)
     t=0;icount=1
     dt = dynpar.dtt
     while t < dynpar.tmax
         t +=dt;icount += 1
-        xv,f=move(xv,dynpar,f,box)
+        xv,f=move(xv,dynpar,f)
         xv=contorno(xv,dynpar)
-        xv, box = build_lists(xv,box)
         # if mod(icount,1000) == 0
         #     #println(t)
         #     V,K,TE = Total_Energy(xv,dynpar)
@@ -322,6 +314,6 @@ xv,dynpar,box=inicia()
 N=length(xv.x)
 println("N=$N tmax=$(dynpar.tmax)")
 f=forc(zeros(N),zeros(N))
-@time evol(xv,f,dynpar,box)
-@time evol(xv,f,dynpar,box)
+@time evol(xv,f,dynpar)
+@time evol(xv,f,dynpar)
 readline()
