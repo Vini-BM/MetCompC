80f97ad2a987f15471b1338466068fea9579f10b
Vini-BM <vinicius.bayne@ufrgs.br>
2024-08-01 15:05:43 -0300

    cleaner code

--- StochasticProcesses/doublePotentialWell.jl
+++ StochasticProcesses/doublePotentialWell.jl
@@ -1,26 +1,44 @@
 using Random
 using Plots
 
-# Parameters
-tmax = 10
-dt = .1
-alpha = 2
-beta = .1
+"""
+Solves the equations of motion for a particle in a double potential well with additive noise.
+Uses Stokes' approximation, in which the particle's inertia is neglected (mass ~ 0).
 
-# Initialize values
-t = 0.0
-x = 6.0
-tlist = [t]
-xlist = [x]
+The potential is given by
+    V(x) = ax^4 - bx^2
 
-# Loop
-while t <= tmax
-    global t += dt
-    r = randn()
-    global x += alpha*x*dt - x^3*dt + beta*r*sqrt(dt)
-    push!(tlist,t)
-    push!(xlist,x)
+The stochastic equation of motion is then given by
+    dX = (\alpha X - X^3)dt + \beta dW(t)
+
+"""
+
+function integrate(x0,tmax,dt,alpha,beta)
+    # Initialize values
+    t = 0.0
+    x = x0
+    tlist = [t]
+    xlist = [x]
+
+    # Loop
+    while t <= tmax
+        t += dt
+        r = randn()
+        x += alpha*x*dt - x^3*dt + beta*r*sqrt(dt)
+        push!(tlist,t)
+        push!(xlist,x)
+    end
+    return tlist, xlist
 end
-println(tlist)
-plot!(tlist,xlist,display=true)
+
+
+# Parameters
+tmax = 5 # integration time
+dt = .01 # integration  timestep
+alpha = 2 # potential parameter
+beta = 1 # noise
+x0 = 6. # initial position
+
+tlist, xlist = integrate(x0,tmax,dt,alpha,beta)
+plot(tlist,xlist,xlabel="t",ylabel="x",title="Particle in double potential well",display=true)
 readline()
